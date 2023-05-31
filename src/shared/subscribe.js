5c5906d0e1d6130e42be73388718e0e567995861
Peter-Paul <ppmunga@gmail.com>
2023-05-31 04:02:25 +0300

    Latest updates

--- src/shared/subscribe.js
+++ src/shared/subscribe.js
@@ -9,9 +9,9 @@ function Subscribe() {
             <h4>Subscribe to our newsletter</h4>
             <p>Get the best high potential coins right into your inbox</p>
 
-            <input style={styles.input} className="form-control form-control-md bg-dark shadow text-center" placeholder="Enter your email" />
+            <input style={styles.input} className="form-control form-control-md bg-light shadow text-center" placeholder="Enter your email" />
             <div className="d-grid gap-2 mt-2">
-                <button className="btn btn-light" type="button">Subscribe</button>
+                <button className="btn btn-dell-blue" type="button">Subscribe</button>
             </div>
 
         </>
